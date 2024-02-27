1f8670bae5e383893c6d36a5e3f73d5578e2c7f6
jarisiber <140783112+jarisiber@users.noreply.github.com>
2024-02-27 11:34:49 +0700

    Add files via upload

---
+++ src/main/java/com/jarisiber/invoicenote/InvoiceNoteApplication.java
@@ -0,0 +1,13 @@
+package com.jarisiber.invoicenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceNoteApplication.class, args);
+	}
+
+}
