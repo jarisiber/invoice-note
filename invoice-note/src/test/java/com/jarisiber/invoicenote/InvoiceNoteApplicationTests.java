308f28c2e4b2d958026616ccad202a37f66158dd
jarisiber <140783112+jarisiber@users.noreply.github.com>
2024-02-27 11:33:05 +0700

    Add files via upload

---
+++ invoice-note/src/test/java/com/jarisiber/invoicenote/InvoiceNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarisiber.invoicenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
